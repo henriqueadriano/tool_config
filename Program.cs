3feb2cc5f539c6d858ac79fe8f57c142b3294632
henrique <henrique.adriano@aspen.edu>
2019-08-16 16:11:45 -0300

    start

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
-using System.Text;
 using System.Xml;
 
 namespace ConsoleApp1
@@ -21,9 +20,7 @@ namespace ConsoleApp1
 
             JObject appConfiguration = (JObject)jsonFile["AppConfiguration"];
 
-            string devConn = (string)appConfiguration["DevelopmentConnection"];
-
-            appConfiguration["DevelopmentConnection"] = BuildStringConnection(objFileds, devConn);
+            appConfiguration["DevelopmentConnection"] = objFileds.StringConn;
 
             FileStream fsOverwrite = new FileStream(objFileds.path, FileMode.Create);
             StreamWriter swOverwrite = new StreamWriter(fsOverwrite);
@@ -113,32 +110,11 @@ namespace ConsoleApp1
 
             foreach (string line in lines)
             {
-                if (line.Contains("S_Server"))
+                if (line.Contains("S_StringConn"))
                 {
                     string value = line.Substring(line.IndexOf("=") + 1).Trim();
                     isValid = value != string.Empty && value != "?";
-                    saObj.server = line.Substring(line.IndexOf("=") + 1).Trim();
-                }
-
-                if (line.Contains("S_Database"))
-                {
-                    string value = line.Substring(line.IndexOf("=") + 1).Trim();
-                    isValid = value != string.Empty && value != "?";
-                    saObj.database = line.Substring(line.IndexOf("=") + 1).Trim();
-                }
-
-                if (line.Contains("S_UserId"))
-                {
-                    string value = line.Substring(line.IndexOf("=") + 1).Trim();
-                    isValid = value != string.Empty && value != "?";
-                    saObj.userId = line.Substring(line.IndexOf("=") + 1).Trim();
-                }
-
-                if (line.Contains("S_Password"))
-                {
-                    string value = line.Substring(line.IndexOf("=") + 1).Trim();
-                    isValid = value != string.Empty && value != "?";
-                    saObj.password = line.Substring(line.IndexOf("=") + 1).Trim();
+                    saObj.StringConn = line.Substring(line.IndexOf("=") + 1).Trim();
                 }
 
                 if (line.Contains("S_PATH_JSON_FILE"))
@@ -160,32 +136,11 @@ namespace ConsoleApp1
 
             foreach (string line in lines)
             {
-                if (line.Contains("SA_Server"))
+                if (line.Contains("SA_StringConn"))
                 {
                     string value = line.Substring(line.IndexOf("=") + 1).Trim();
                     isValid = value != string.Empty && value != "?";
-                    saObj.server = line.Substring(line.IndexOf("=") + 1).Trim();
-                }
-
-                if (line.Contains("SA_Database"))
-                {
-                    string value = line.Substring(line.IndexOf("=") + 1).Trim();
-                    isValid = value != string.Empty && value != "?";
-                    saObj.database = line.Substring(line.IndexOf("=") + 1).Trim();
-                }
-
-                if (line.Contains("SA_UserId"))
-                {
-                    string value = line.Substring(line.IndexOf("=") + 1).Trim();
-                    isValid = value != string.Empty && value != "?";
-                    saObj.userId = line.Substring(line.IndexOf("=") + 1).Trim();
-                }
-
-                if (line.Contains("SA_Password"))
-                {
-                    string value = line.Substring(line.IndexOf("=") + 1).Trim();
-                    isValid = value != string.Empty && value != "?";
-                    saObj.password = line.Substring(line.IndexOf("=") + 1).Trim();
+                    saObj.StringConn = line.Substring(line.IndexOf("=") + 1).Trim();
                 }
 
                 if (line.Contains("SA_PATH_JSON_FILE"))
@@ -197,46 +152,6 @@ namespace ConsoleApp1
             }
             saObj.IsValid = isValid;
             return saObj;
-        }
-
-        private static string BuildStringConnection(ObjFields info, string devConn)
-        {
-            string[] listInfo = devConn.Split(";");
-
-            StringBuilder sb = new StringBuilder();
-
-            foreach (string item in listInfo)
-            {
-                if (item.Split('=')[0].Equals("Server"))
-                {
-                    sb.Append(item.Split('=')[0]);
-                    sb.Append("=");
-                    sb.Append(info.server);
-                    sb.Append(";");
-                }
-                if (item.Split('=')[0].Equals("Database"))
-                {
-                    sb.Append(item.Split('=')[0]);
-                    sb.Append("=");
-                    sb.Append(info.database);
-                    sb.Append(";");
-                }
-                if (item.Split('=')[0].Equals("User Id"))
-                {
-                    sb.Append(item.Split('=')[0]);
-                    sb.Append("=");
-                    sb.Append(info.userId);
-                    sb.Append(";");
-                }
-                if (item.Split('=')[0].Equals("Password"))
-                {
-                    sb.Append(item.Split('=')[0]);
-                    sb.Append("=");
-                    sb.Append(info.password);
-                }
-            }
-
-            return sb.ToString();
         }
 
     }
